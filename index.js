5577aa341055876809811afd47f710888453397d
John Vawer <john.vawer@dpdgroup.co.uk>
2019-07-07 14:48:18 +0100

    Added better error handling, installed mocha package ready to write tests

--- index.js
+++ index.js
@@ -4,17 +4,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const routes = require("./lib/routes");
+const {
+  catchAll,
+  errorHandler,
+} = require("./lib/middleware");
 
 app.use(express.json());
 app.use(routes);
 
-app.use((err, req, res, next) => {
-  if (err.httpStatusCode) {
-    return res.status(err.httpStatusCode).json({ success: false, error: err.message });
-  }
-
-  return res.status(500).json({ success: false, error: "An unexpected error occured" });
-});
+app.use(catchAll);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
